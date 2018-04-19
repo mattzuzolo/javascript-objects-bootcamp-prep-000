294a55c9359d46940daf461123ab2c09032864ca
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-19 00:31:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,12 +1,23 @@
-var playlist = new Object ({ artistName : "songTitle" });
+var playlist = { artistName: "songTitle" };
 
+
+
+//This function destrucively adds the song and artist as a key-value pair to the playlist object
 function updatePlaylist (playlist, artistName, songTitle) {
+                        //obj, key, value
+  //obj [key] = value;
+  playlist [artistName] = songTitle;
   
-  playlist[artistName] = songTitle
+  //return obj;
   return playlist;
 }
 
+
+
+
+
 function removeFromPlaylist (playlist, artistName) {
+  
   delete playlist[artistName];
   return playlist;
 }
